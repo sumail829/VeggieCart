a18f2f1220c9d83f4c9d28f9a06f5313417dd312
suman <sumail829>
2024-10-15 13:21:35 +0545

    updated card

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,45 @@ import { Card } from "@/components/Card";
 import { Navbar } from "@/components/Navbar";
 import Image from "next/image";
 
-const card_attribute={
+const card_attribute=[
+  {
+    id:1,
   title:"The Coldest Sunset",
   imgDescription:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
   imgAlt:"the sunset",
-  imgSrc:"/test.jpg"
+  imgSrc:"/test.jpg",},
 
+  {
+    id:2,
+    title:"The Coldest Sunset",
+    imgDescription:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    imgAlt:"the sunset",
+    imgSrc:"/test.jpg",},
 
-}
+    {
+      id:3,
+      title:"The Coldest Sunset",
+      imgDescription:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+      imgAlt:"the sunset",
+      imgSrc:"/test.jpg",},
+
+  
+];
 
 export default function Home() {
   return (
     <div>
      <Navbar></Navbar>
-     <Card 
-     title={card_attribute.title}
-     imgDescription={card_attribute.imgDescription}
-     imgAlt={card_attribute.imgAlt}
-     imgSrc={card_attribute.imgSrc}></Card>
+     <div>
+      {card_attribute.map(({id, title,imgDescription,imgAlt,imgSrc})=>(
+        <Card
+        title={title}
+        key={id}
+        imgDescription={imgDescription}
+        imgAlt={imgAlt}
+        imgSrc={imgSrc}></Card>
+      ))}
+      </div>
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,11 @@ export const Navbar =()=> {
          
         <div className="inline-flex items-center  gap-x-1.5 font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500 active:text-blue-700">
         <DropdownMenu>
-  <DropdownMenuTrigger>Home</DropdownMenuTrigger>
+  <DropdownMenuTrigger className="flex flex-cols item-center">Home
+  <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"/>
+        </svg>
+  </DropdownMenuTrigger>
   <DropdownMenuContent>
     <DropdownMenuLabel>My Account</DropdownMenuLabel>
     <DropdownMenuSeparator />
@@ -36,9 +40,7 @@ export const Navbar =()=> {
   </DropdownMenuContent>
 </DropdownMenu>
 
-        <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"/>
-        </svg>
+      
        
        </div>
 
